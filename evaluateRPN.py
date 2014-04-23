c9806cc2509332cb5ff7dd297afa293611159a3d
judening <jning@ngpvan.com>
2014-04-23 18:27:24 -0400

    Create evaluateRPN.py

---
+++ evaluateRPN.py
@@ -0,0 +1,24 @@
+class Solution:
+    # @param tokens, a list of string
+    # @return an integer
+    def evalRPN(self, tokens):
+        stack = []
+        operations = ['+','-','*','/']
+        if len(tokens) < 2:
+            return int(tokens[0])
+        else:
+            for i in tokens:
+                if i not in operations:
+                    stack.append(i)
+                else:
+                    a = stack.pop(-1)
+                    b = stack.pop(-1)
+                    if i == '+':
+                        stack.append(int(b) + int(a))
+                    elif i == '-':
+                        stack.append(int(b) - int(a))
+                    elif i == '*':
+                        stack.append(int(b) * int(a))
+                    else:
+                        stack.append(int(float(int(b)) / int(a)))
+            return stack.pop(0)
