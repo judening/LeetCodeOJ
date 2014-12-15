d7a175d1d9c765fd23c616c7317778a10625d752
Jude Ning <jning@ngpvan.com>
2014-12-15 12:48:12 -0500

    reverseWords.py summarized = 38

--- stringManipulation/reverseWords.py
+++ stringManipulation/reverseWords.py
@@ -5,9 +5,8 @@ class Solution:
         a = s.split(" ")
         newStr = ""
         if len(a) == 0 :
-            print s
+            return s
         else:
             for i in range(len(a)-1,-1,-1):
                 newStr = newStr + " " + a[i]
-            
-            print newStr
+            return newStr
